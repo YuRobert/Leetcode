50a6a3c18d0a2c4c8ccbce2b6917b4be402f4e26
YuRobert <111642519+YuRobert@users.noreply.github.com>
2023-10-23 21:12:39 +0800

    leetcode2023.10.23

--- src/leetcode/leetcode343/Solution.java
+++ src/leetcode/leetcode343/Solution.java
@@ -6,4 +6,15 @@ package leetcode.leetcode343;
  */
 public class Solution
 {
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        return 0;
+    }
+
+    public static void main(String[] args)
+    {
+        Solution solution = new Solution();
+        int i = solution.integerBreak(6);
+        System.out.println(i);
+    }
 }
